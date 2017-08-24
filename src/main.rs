5a55f186f9323a2c2e9eeb93e00a3e59eba1637f
Antônio Roberto <forevertonny@gmail.com>
2017-08-24 19:21:59 -0300

    use dispatcher / cli functions on main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 mod cli;
+mod dispatcher;
+
+use dispatcher::{listener, DispatcherConfig};
+use cli::{create_cli_app};
+
 fn main() {
+    listener(
+        &DispatcherConfig::from_matches(
+            &create_cli_app().get_matches()));
 }
