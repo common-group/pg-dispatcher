461c4e008dffe594ef1172603d8d2c1e32173cfc
Antônio Roberto <forevertonny@gmail.com>
2017-09-20 17:24:22 -0300

    Adjust cli to accept redis-uri and mode

--- src/cli.rs
+++ src/cli.rs
@@ -8,7 +8,17 @@ pub fn create_cli_app<'a, 'b>() -> App<'a, 'b> {
         .arg(Arg::with_name("db-uri")
              .long("db-uri")
              .help("database connection string postgres://user:pass@host:port/dbname")
+             .required(false)
+             .takes_value(true))
+        .arg(Arg::with_name("redis-uri")
+             .long("redis-uri")
+             .help("redis connection string redis://localhost:6379")
              .required(true)
+             .takes_value(true))
+        .arg(Arg::with_name("mode")
+             .long("mode")
+             .help("consumer, producer or both (default both)")
+             .required(false)
              .takes_value(true))
         .arg(Arg::with_name("channel")
              .long("channel")
@@ -36,11 +46,15 @@ mod tests {
         let matches = create_cli_app()
             .get_matches_from(vec![
                               "pg-dispatch", "--db-uri", "foodb",
+                              "--redis-uri", "redis://localhost:6379",
+                              "--mode", "both"
                               "--channel", "foochan",
                               "--exec", "sh test.sh",
                               "--workers", "5"]);
 
         assert_eq!("foodb", matches.value_of("db-uri").unwrap());
+        assert_eq!("redis://localhost:6379", matches.value_of("redis-uri").unwrap());
+        assert_eq!("both", matches.value_of("mode").unwrap());
         assert_eq!("foochan", matches.value_of("channel").unwrap());
         assert_eq!("sh test.sh", matches.value_of("exec").unwrap());
         assert_eq!("5", matches.value_of("workers").unwrap());
