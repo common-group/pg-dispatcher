329ddc43f1bfeca3146e35a57acaef5249392622
Antônio Roberto <forevertonny@gmail.com>
2018-08-24 09:01:33 -0300

    progress

--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -47,93 +47,100 @@ impl <'a>DispatcherPool<'a> {
 
 impl Dispatcher {
     pub fn from_config(config: &DispatcherConfig) -> Dispatcher {
-        Dispatcher {
+        let dispatcher = Dispatcher {
             config: config.clone()
+        };
+        debug!("Dispatcher from config {:?}", dispatcher);
+        return dispatcher;
+    }
+
+    pub fn start_consumer(&self) -> Option<thread::JoinHandle<()>> {
+        {
+            if(!config.consumer.is_some()) {
+                debug!("not have a consumer configuration. customer not started");
+                return None;
+            };
+
+            let redis_client = redis::Client::open(config.redis.uri.as_str()).unwrap();
+            let redis_conn = redis_client.get_connection().unwrap();
+            let pending_set = format!("dispatcher:{}:pending_set", &config.db_channel);
+            let processing_set = format!("dispatcher:{}:processing_set", &config.db_channel);
+            let done_set = format!("dispatcher:{}:done_set", &config.db_channel);
+
+            let handler = thread::spawn(move||{
+                let pool = ThreadPool::new(
+                    config.max_threads,
+                    config.command_vector.clone());
+
+                println!(
+                    "[pg-dispatcher-consumer] Start consumer for payloads of channel {}",
+                    config.db_channel);
+
+                loop {
+                    let guard_idle_counter = pool.idle_counter.clone();
+                    let guard_counter: usize;
+
+                    {
+                        let counter = match guard_idle_counter.lock() {
+                            Ok(count) => count,
+                            Err(p) => p.into_inner()
+                        };
+
+                        guard_counter = *counter;
+                    }
+
+                    if let Ok(worker_output) = pool.workers_channel.try_recv() {
+                        match worker_output {
+                            WorkerMessage::ProgramNotFound(b64_key)
+                                | WorkerMessage::StdinFailed(b64_key) => {
+                                let _ : Result<(),_> = redis_conn.
+                                    srem(processing_set.clone(), b64_key);
+                            },
+                            WorkerMessage::DoneTask(b64_key) => {
+                                // add to done task
+                                // TODO: add some task to cleanup the done set
+                                let _ : Result<(), _> = redis_conn.
+                                    sadd(done_set.clone(), b64_key.clone());
+
+                                // remove from pending set
+                                let _ : Result<(),_> = redis_conn.
+                                    srem(pending_set.clone(), b64_key.clone());
+
+                                // remove from processing set
+                                let _ : Result<(),_> = redis_conn.
+                                    srem(processing_set.clone(), b64_key.clone());
+                            }
+                        }
+                    }
+
+                    let diff_result : Result<Vec<String>, _> = redis_conn
+                        .sdiff(&[pending_set.clone(), processing_set.clone()]);
+
+                    if let Ok(diff) = diff_result {
+                        for (i, key) in diff.iter().enumerate() {
+                            if i+1 > guard_counter { break; }
+                            let decoded = base64::decode(&key).unwrap();
+
+                            if let Ok(payload) = str::from_utf8(&decoded) {
+                                match redis_conn.sadd(processing_set.clone(), key) {
+                                    Ok(1) => {
+                                        println!("[pg-dispatcher-consumer] start processing key {}", &key);
+                                        pool.execute(payload.to_string())
+                                    },
+                                    _ => {}
+                                };
+                            }
+                        }
+                    }
+
+                    thread::sleep(time::Duration::from_millis(100));
+                }
+            });
+
+            return handler;
         }
     }
-//
-//    pub fn start_consumer(&self, redis_client: redis::Client) -> thread::JoinHandle<()> {
-//        {
-//            let config = self.config.clone();
-//            let redis_conn = redis_client.get_connection().unwrap();
-//            let pending_set = format!("dispatcher:{}:pending_set", &config.db_channel);
-//            let processing_set = format!("dispatcher:{}:processing_set", &config.db_channel);
-//            let done_set = format!("dispatcher:{}:done_set", &config.db_channel);
-//
-//            let handler = thread::spawn(move||{
-//                let pool = ThreadPool::new(
-//                    config.max_threads,
-//                    config.command_vector.clone());
-//
-//                println!(
-//                    "[pg-dispatcher-consumer] Start consumer for payloads of channel {}",
-//                    config.db_channel);
-//
-//                loop {
-//                    let guard_idle_counter = pool.idle_counter.clone();
-//                    let guard_counter: usize;
-//
-//                    {
-//                        let counter = match guard_idle_counter.lock() {
-//                            Ok(count) => count,
-//                            Err(p) => p.into_inner()
-//                        };
-//
-//                        guard_counter = *counter;
-//                    }
-//
-//                    if let Ok(worker_output) = pool.workers_channel.try_recv() {
-//                        match worker_output {
-//                            WorkerMessage::ProgramNotFound(b64_key)
-//                                | WorkerMessage::StdinFailed(b64_key) => {
-//                                let _ : Result<(),_> = redis_conn.
-//                                    srem(processing_set.clone(), b64_key);
-//                            },
-//                            WorkerMessage::DoneTask(b64_key) => {
-//                                // add to done task
-//                                // TODO: add some task to cleanup the done set
-//                                let _ : Result<(), _> = redis_conn.
-//                                    sadd(done_set.clone(), b64_key.clone());
-//
-//                                // remove from pending set
-//                                let _ : Result<(),_> = redis_conn.
-//                                    srem(pending_set.clone(), b64_key.clone());
-//
-//                                // remove from processing set
-//                                let _ : Result<(),_> = redis_conn.
-//                                    srem(processing_set.clone(), b64_key.clone());
-//                            }
-//                        }
-//                    }
-//
-//                    let diff_result : Result<Vec<String>, _> = redis_conn
-//                        .sdiff(&[pending_set.clone(), processing_set.clone()]);
-//
-//                    if let Ok(diff) = diff_result {
-//                        for (i, key) in diff.iter().enumerate() {
-//                            if i+1 > guard_counter { break; }
-//                            let decoded = base64::decode(&key).unwrap();
-//
-//                            if let Ok(payload) = str::from_utf8(&decoded) {
-//                                match redis_conn.sadd(processing_set.clone(), key) {
-//                                    Ok(1) => {
-//                                        println!("[pg-dispatcher-consumer] start processing key {}", &key);
-//                                        pool.execute(payload.to_string())
-//                                    },
-//                                    _ => {}
-//                                };
-//                            }
-//                        }
-//                    }
-//
-//                    thread::sleep(time::Duration::from_millis(100));
-//                }
-//            });
-//
-//            return handler;
-//        }
-//    }
-//
+
 //    pub fn start_producer(&self, pg_conn: postgres::Connection, redis_client: redis::Client) -> thread::JoinHandle<()> {
 //        {
 //            let config = self.config.clone();
